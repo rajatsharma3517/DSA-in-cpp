3a58e74be6219e9ee823a977cb8413ee0d5b0d21
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-08-09 20:01:34 +0530

    Add array program

---
+++ codes/array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxi = arr[0];
+
+    for(int i = 1; i < n; i++) {
+
+        if(arr[i] > maxi) {
+            maxi = arr[i];
+        }
+
+    }
+
+    cout << maxi;
+
+    return 0;
+}
